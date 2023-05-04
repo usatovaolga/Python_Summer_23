4c6f0fc2f2ba7a4903f3193cf92982a8df2f5044
usatova_olga <olyausatova97@mail.ru>
2023-05-04 15:08:36 +0300

    Task_3_3

---
+++ Task_3_3.py
@@ -0,0 +1,12 @@
+s = input("Введите предложение ->   ") #hi how are you
+lst=[]
+lst=s.split()
+mlen=0
+mlst=[]
+for i in lst:
+    if len(i)>mlen: mlen=len(i)
+    s=i
+for i in lst:
+    if len(i)==mlen: mlst.append(i)
+print(f"Самое длинное первое слово - {mlst[0]}")
+print("Все самые длинные слова - ", ', '.join(mlst))
