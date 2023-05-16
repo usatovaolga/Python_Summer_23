fb89bd0ec294ce41c0735d7a0bd2f9fb850a60ba
usatova_olga <olyausatova97@mail.ru>
2023-05-17 01:10:18 +0300

    Task_7_2

---
+++ Task_7_2.py
@@ -0,0 +1,14 @@
+
+y=[]
+str1= input("Введите строку-")
+n=int(input("Введите шаг-"))
+def code(string, n):
+    s = 'abcdefghijklmnopqrstuvwxyz'
+    for i in string:
+        if i in s:
+            t=s[(s.find(i)+n)%len(s)]
+            y.append(t)
+        else:
+            y.append(i)
+    print(f"Зашифрованая строка- {''.join(y)}")
+code(str1,n)
