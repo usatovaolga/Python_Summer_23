5d2f2cca22011c2ed4c1edc0038dbed644c4e5cb
usatova_olga <olyausatova97@mail.ru>
2023-05-19 18:00:54 +0300

    Task_8_2

--- Task_8_2.py
+++ Task_8_2.py
@@ -1,2 +1,9 @@
 s = [[1,2,3],[2,334,24,6],[8,6]]
-print(sorted(s,key= lambda s:len(''.join(map(str,s)))))
+t=[1,2,3]
+s2=[]
+for i in s:
+    t=sorted(i,reverse=True)
+    s2.append(t)
+print(s2)
+print(sorted(s2,key= lambda s:len(''.join(map(str,s)))))
+#print(sorted(t,reverse=True))
