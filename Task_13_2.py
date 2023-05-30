43fae652779618c0e5585d701d5e9ad7c8195f29
usatova_olga <olyausatova97@mail.ru>
2023-05-30 13:33:48 +0300

    Task_13_2

---
+++ Task_13_2.py
@@ -0,0 +1,13 @@
+def p(n):
+    for i in range(n):
+        d=len(str(i))
+        s = d // 2
+        if d==1 and i!=0:
+            yield i
+        elif d%2==0 and str(i)[:s] == str(i)[s:][::-1]:
+            yield i
+        elif d%3==0 and str(i)[:s] == str(i)[s+1:][::-1]:
+            yield i
+posl=p(int(input('Введите число ограничения->')))
+for b in posl:
+    print(b,end=' ')
