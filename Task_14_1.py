375040503461333c2f9e83ddf41dcdb384186f71
usatova_olga <olyausatova97@mail.ru>
2023-06-02 11:11:55 +0300

    Task_14_1

--- Task_14_1.py
+++ Task_14_1.py
@@ -10,6 +10,6 @@ while True:
         print("Вы ввели не натуральное число, попробуйте снова ->")
 def k_c(n):
     if n>=10:
-        return 1+k_c(n/10)
+        return 1+k_c(n//10)
     return 1
 print(f"Кол-во цифр натурального числа = {k_c(n)}")
