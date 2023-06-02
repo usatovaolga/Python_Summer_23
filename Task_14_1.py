0500664df46bfffcd6620b7ee87f7189be4604e2
usatova_olga <olyausatova97@mail.ru>
2023-06-02 09:36:27 +0300

    Task_14_1

---
+++ Task_14_1.py
@@ -0,0 +1,15 @@
+n = int(input('Введите число ->'))
+class NotNature(ValueError):
+    pass
+while True:
+    try:
+        if n>=1:
+            break
+        else: raise NotNature('Не натуральное')
+    except:
+        n = int(input("Вы ввели не натуральное число, попробуйте снова ->"))
+def k_c(n):
+    if n>=10:
+        return 1+k_c(n/10)
+    return 1
+print(f"Кол-во цифр в натуральном числе = {k_c(n)}")
