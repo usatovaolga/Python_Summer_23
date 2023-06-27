deb8fb4528b5416c4ebeaeedcc97a5a6bca12913
usatova_olga <olyausatova97@mail.ru>
2023-06-27 23:31:30 +0300

    Task_24_3

---
+++ Task_24_3.py
@@ -0,0 +1,11 @@
+ss='(()())()(())'
+sinp=input("Введите строку ->").replace(" ", "")
+def skobka(s):
+    t = 0
+    for i in s:
+        if t==-1: return False
+        elif i=='(': t+=1
+        else: t-=1
+    if t==1: return False
+    else: return True
+print(skobka(sinp))
