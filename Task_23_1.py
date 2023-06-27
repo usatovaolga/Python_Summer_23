96e8cfa48c8807743748355d2a6f1d49ab7f85b0
usatova_olga <olyausatova97@mail.ru>
2023-06-27 22:19:01 +0300

    Task_23_1

--- Task_23_1.py
+++ Task_23_1.py
@@ -1,9 +1,10 @@
 s='aabbccddcc'
 m=''
-ss=input('Введите строку ->')
+ss = input('Введите строку ->')
 for i in range(len(ss)):
     for j in range(len(ss),i,-1):
-        if ss[i:j]==s[i:j][::-1]:
+        if ss[i:j]==ss[i:j][::-1]:
             if len(m)<len(ss[i:j]):
                 m=ss[i:j]
+
 print(m)
