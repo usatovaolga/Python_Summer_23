a85d87075b688683bc59329ee3abf6aee5be4974
usatova_olga <olyausatova97@mail.ru>
2023-06-30 19:02:10 +0300

    Task_25_2

--- Task_25_2.py
+++ Task_25_2.py
@@ -1,8 +1,11 @@
 s=[-1,0,1,3,5,4,7,9,10]
-t=s[0]
+ss = list(map(int, input().split()))
+t=ss[0]
 tt=[]
-for i in s[1:]:
+for i in ss[1:]:
     if i!=t+1:
         tt.append(i)
     t=i
 print(tt)
+
+#print([s[i] for i in range(1,len(s)) if s[i-1]+1 != s[i]])
