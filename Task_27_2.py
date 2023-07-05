e72afcef658fac42726d58d9f661386f5f175369
usatova_olga <olyausatova97@mail.ru>
2023-07-05 19:21:12 +0300

    Task_27_2

--- Task_27_2.py
+++ Task_27_2.py
@@ -29,22 +29,22 @@ print(a.total)
 
 #########
 
-class Item:
-    def __init__(self,name,price,quantity):
-        self.name=name
-        self.price = price
-        self.quantity=quantity
-
-    def __getattribute__(self, attr):
-        if attr=='name':
-            return object.__getattribute__(self,attr).titel()
-        elif attr=='price':
-            return 2*object.__getattribute__(self,attr)
-        else:
-            return object.__getattribute__(self,attr)
-
-    def __getattr__(self, attr):
-        if attr=='total':
-            return self.price*self.quantity
-        else:
-            raise AttributeError
+# class Item:
+#     def __init__(self,name,price,quantity):
+#         self.name=name
+#         self.price = price
+#         self.quantity=quantity
+#
+#     def __getattribute__(self, attr):
+#         if attr=='name':
+#             return object.__getattribute__(self,attr).titel()
+#         elif attr=='price':
+#             return 2*object.__getattribute__(self,attr)
+#         else:
+#             return object.__getattribute__(self,attr)
+#
+#     def __getattr__(self, attr):
+#         if attr=='total':
+#             return self.price*self.quantity
+#         else:
+#             raise AttributeError
