b4f7413b324f349605f47a58e090bfc27ed5ced2
usatova_olga <olyausatova97@mail.ru>
2023-07-05 19:17:51 +0300

    Task_27_2

--- Task_27_2.py
+++ Task_27_2.py
@@ -12,3 +12,37 @@ a=Item('a',500,5)
 b=Item('B',1000,3)
 print(a.name)
 print(a.total)
+#############
+# class Item:
+#     def __init__(self,name,price,quantity):
+#         self._name=name
+#         self.price = price
+#         self.quantity=quantity
+#
+#     @property
+#     def total(self):
+#         return self.price*self.quantity
+#
+#     @property
+#     def name(self):
+#         return self._name.titel()
+
+#########
+
+class Item:
+    def __init__(self,name,price,quantity):
+        self.name=name
+        self.price = price
+        self.quantity=quantity
+
+    def __getattribute__(self, attr):
+        if attr=='name':
+            return object.__getattribute__(self,attr).titel()
+        else:
+            return object.__getattribute__(self,attr)
+
+    def __getattr__(self, attr):
+        if attr=='total':
+            return self.price*self.quantity
+        else:
+            raise AttributeError
